244e80f3645b09b58e3174df342102422b0665bd
zhangshuping <273647953@qq.com>
2017-07-10 22:00:27 +0800

    firstly commit

---
+++ js/scripts.js
@@ -0,0 +1,217 @@
+
+$(document).ready(function(){
+			$("#flip1").click(function(){
+				$("#panel1").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip2").click(function(){
+				$("#panel2").slideToggle("fast");
+				});
+			
+		});
+
+$(document).ready(function(){
+			$("#flip3").click(function(){
+				$("#panel3").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip4").click(function(){
+				$("#panel4").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip5").click(function(){
+				$("#panel5").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip6").click(function(){
+				$("#panel6").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip7").click(function(){
+				$("#panel7").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip8").click(function(){
+				$("#panel8").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip9").click(function(){
+				$("#panel9").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip10").click(function(){
+				$("#panel10").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip11").click(function(){
+				$("#panel11").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip12").click(function(){
+				$("#panel12").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip13").click(function(){
+				$("#panel13").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip14").click(function(){
+				$("#panel14").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip15").click(function(){
+				$("#panel15").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip16").click(function(){
+				$("#panel16").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip17").click(function(){
+				$("#panel17").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip18").click(function(){
+				$("#panel18").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip19").click(function(){
+				$("#panel19").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip20").click(function(){
+				$("#panel20").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip21").click(function(){
+				$("#panel21").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip22").click(function(){
+				$("#panel22").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip23").click(function(){
+				$("#panel23").slideToggle("fast");
+				});
+			
+		});
+$(document).ready(function(){
+			$("#flip24").click(function(){
+				$("#panel24").slideToggle("fast");
+				});
+			
+		});$(document).ready(function(){
+			$("#flip25").click(function(){
+				$("#panel25").slideToggle("fast");
+				});
+			
+		});
+		$(document).ready(function(){
+			$("#flip26").click(function(){
+				$("#panel26").slideToggle("fast");
+				});
+			
+		});
+		$(document).ready(function(){
+			$("#flip27").click(function(){
+				$("#panel27").slideToggle("fast");
+				});
+			
+		});
+		$(document).ready(function(){
+			$("#flip28").click(function(){
+				$("#panel28").slideToggle("fast");
+				});
+			
+		});
+		$(document).ready(function(){
+			$("#flip29").click(function(){
+				$("#panel29").slideToggle("fast");
+				});
+			
+		});
+		$(document).ready(function(){
+			$("#flip30").click(function(){
+				$("#panel30").slideToggle("fast");
+				});
+			
+		});
+		$(document).ready(function(){
+			$("#flip31").click(function(){
+				$("#panel31").slideToggle("fast");
+				});
+			
+		});
+		$(document).ready(function(){
+			$("#flip32").click(function(){
+				$("#panel32").slideToggle("fast");
+				});
+			
+		});
+		$(document).ready(function(){
+			$("#flip33").click(function(){
+				$("#panel33").slideToggle("fast");
+				});
+			
+		});
+		$(document).ready(function(){
+			$("#flip34").click(function(){
+				$("#panel34").slideToggle("fast");
+				});
+			
+		});
+		$(document).ready(function(){
+			$("#flip35").click(function(){
+				$("#panel35").slideToggle("fast");
+				});
+			
+		});
+		$(document).ready(function(){
+			$("#flip36").click(function(){
+				$("#panel36").slideToggle("fast");
+				});
+			
+		});
